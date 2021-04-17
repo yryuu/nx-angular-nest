8862a0c88d1adc00d0a8d00f90236c6cc8999391
yryuu <yryuuyamazaki@gmail.com>
2021-04-17 19:52:56 +0900

    routing 追加

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), HttpClientModule],
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
