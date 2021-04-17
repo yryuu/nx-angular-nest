79fe287416be2260835c98688680c287802d94a7
yryuu <yryuuyamazaki@gmail.com>
2021-04-17 19:55:15 +0900

    routing 追加

--- apps/client/src/app/app.routing.module.ts
+++ apps/client/src/app/app.routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from './app.component'
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component:AppComponent},
-  { path: 'test', component:AppComponent}
+  { path: '', component: AppComponent },
+  { path: 'test', component: AppComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
