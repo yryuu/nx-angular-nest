04853c4ee22f59f9e812fd3ee22f77301ef61f6b
yryuu <yryuuyamazaki@gmail.com>
2021-04-24 14:48:19 +0900

    init clinet ui

--- apps/client/src/app/app.routing.module.ts
+++ apps/client/src/app/app.routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './component/screen/chat/chat.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
-  { path: 'test', component: AppComponent },
+  { path: 'test', component: ChatComponent },
 ];
 
 @NgModule({
